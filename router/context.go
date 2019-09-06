624923645810b61b2b06fa422f939bbbcb04554e
Ilia Shchepin <ischepin@optherium.com>
2019-09-06 11:48:20 +0300

    Add log wrappers to router.context

--- router/context.go
+++ router/context.go
@@ -1,6 +1,9 @@
 package router
 
 import (
+	"fmt"
+	"runtime"
+	"strings"
 	"time"
 
 	"github.com/hyperledger/fabric/core/chaincode/lib/cid"
@@ -80,6 +83,13 @@ type (
 
 		Event() state.Event
 		UseEvent(state.Event) state.Event
+
+		Errorf(string, ...interface{})
+		Debugf(string, ...interface{})
+		Criticalf(string, ...interface{})
+		Noticef(string, ...interface{})
+		Infof(string, ...interface{})
+		Warningf(string, ...interface{})
 	}
 
 	context struct {
@@ -248,4 +258,40 @@ func (c *context) Get(key string) interface{} {
 
 func (c *context) SetEvent(name string, payload interface{}) error {
 	return c.Event().Set(name, payload)
+}
+
+func (c *context) Errorf(format string, args ...interface{}) {
+	c.logger.Errorf(fmt.Sprintf("[%s]: %s", getCaller(), format), args...)
+}
+
+func (c *context) Debugf(format string, args ...interface{}) {
+	c.logger.Debugf(fmt.Sprintf("[%s]: %s", getCaller(), format), args...)
+}
+
+func (c *context) Infof(format string, args ...interface{}) {
+	c.logger.Infof(fmt.Sprintf("[%s]: %s", getCaller(), format), args...)
+}
+
+func (c *context) Warningf(format string, args ...interface{}) {
+	c.logger.Warningf(fmt.Sprintf("[%s]: %s", getCaller(), format), args...)
+}
+
+func (c *context) Criticalf(format string, args ...interface{}) {
+	c.logger.Criticalf(fmt.Sprintf("[%s]: %s", getCaller(), format), args...)
+}
+
+func (c *context) Noticef(format string, args ...interface{}) {
+	c.logger.Noticef(fmt.Sprintf("[%s]: %s", getCaller(), format), args...)
+}
+
+func getCaller() string {
+	pc := make([]uintptr, 15)
+	n := runtime.Callers(3, pc)
+	frames := runtime.CallersFrames(pc[:n])
+	frame, _ := frames.Next()
+	fileSplit := strings.Split(frame.File, "/")
+	file := fileSplit[len(fileSplit)-1]
+	functionSplit := strings.Split(frame.Function, ".")
+	function := functionSplit[len(functionSplit)-1]
+	return fmt.Sprintf("%s:%d->%s", file, frame.Line, function)
 }
